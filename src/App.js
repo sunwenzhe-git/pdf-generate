3502122baea06091f1877bee578ee1b0a009854b
sunwenzhe-git <13836705550@163.com>
2023-10-18 13:55:11 +0800

    test: 💍 env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import { useRef, useState, useDeferredValue } from "react";
 import { PDFViewer } from "@react-pdf/renderer";
-import { useDeepCompareEffect } from "ahooks";
+import { useDeepCompareEffect, useRequest } from "ahooks";
 import PanelContainer from "./panelComponents/PanelContainer";
 import PDFContainer from "./components/PDFContainer";
 import { pdf1 } from "./utils";
+const render = (template, data) => {
+  const regex = /{{([\d\D]*?)}}/g;
+  const replacedTemplate = template.replace(regex, ($0, $1) => {
+    try {
+      return eval(`with(data){${$1}}`);
+    } catch (error) {
+      console.error(error);
+      return "";
+    }
+  });
+  return replacedTemplate;
+};
 
+function getData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const data = {
+        A: "慢性疾病共同治理先导计划",
+        B: "Chronic Disease Co-Care Pilot Schema",
+      };
+      resolve(data);
+    }, 1000);
+  });
+}
 const INIT_PDF_CONFIG = {
   h1: 24,
   h2: 18,
@@ -17,20 +40,26 @@ const INIT_PDF_CONFIG = {
   width_3: 180,
 };
 window.__INIT_PDF_CONFIG__ = INIT_PDF_CONFIG;
-function formData(config) {
+function formData(config, data) {
+  console.log(render(JSON.stringify(config), data), data, "config");
   return config;
-  // config.data?.map()
 }
 const App = () => {
   const iframeRef = useRef(null);
   const [config, setConfig] = useState(null);
-  const data = useDeferredValue(config);
+  const { data, error, loading } = useRequest(getData);
+  const configData = useDeferredValue(config);
   useDeepCompareEffect(() => {
     window.__INIT_PDF_CONFIG__ = data?.__INIT_PDF_CONFIG__
       ? data.__INIT_PDF_CONFIG__
       : INIT_PDF_CONFIG;
   }, [data?.__INIT_PDF_CONFIG__]);
-
+  if (loading) {
+    return <>加载中...</>;
+  }
+  if (error) {
+    return <>{JSON.stringify(error)}</>;
+  }
   return (
     <div style={{ height: "100vh", display: "flex", flexDirection: "row" }}>
       <div style={{ flex: 1, padding: 30 }}>
@@ -44,7 +73,7 @@ const App = () => {
             innerRef={iframeRef}
             style={{ width: "100%", height: "100%" }}
           >
-            <PDFContainer config={formData(data)} />
+            <PDFContainer config={formData(configData, data)} />
           </PDFViewer>
         </div>
       </div>
